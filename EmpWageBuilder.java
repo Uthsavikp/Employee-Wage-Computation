0ec0a54864920acf0414f60e8f4ec232783c4924
uthsa <uthsavi29@gmail.com>
2020-11-10 20:49:30 +0530

    Modified:To Calculate Daily Employee Wage

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -4,12 +4,18 @@ public class EmpWageBuilder {
 		System.out.println("Welcome To Employee Wage Computation");
 				// Constants
 		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		// Variables
+		int empHrs = 0;
+		int empWage = 0;
 					// Computation
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == IS_FULL_TIME)
-			System.out.println("Employee Is Present");
+			empHrs = 8;
 		else
-			System.out.println("Employee Is Absent");
+			empHrs = 0;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage:" + empWage);
 				
 	}
 }
